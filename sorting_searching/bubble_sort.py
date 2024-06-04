9cb59c8c11bbe6af84dc1ffe5e045fb9e0b4e4f1
ChiaraBecht <c.becht@st.hanze.nl>
2024-06-04 10:10:50 +0200

    added early termination criterion

--- sorting_searching/bubble_sort.py
+++ sorting_searching/bubble_sort.py
@@ -14,9 +14,17 @@ def conduct_bubble_sort(data):
         print(data)
         data_len = len(data)
         for i in range(data_len - 1): # skip last pass where we only look on the first element
+            print(i)
+            swap = {"yes": 0, "no": 0}
             for j in range(0, data_len - i - 1): # shortening the comparisons omitting the last elements that where sorted already
                 if data[j] > data[j+1]:
+                    swap["yes"] += 1
                     data[j], data[j+1] = data[j+1], data[j]
+                else:
+                    swap["no"] += 1
+            if swap["yes"] == 0:
+                print(data)
+                return data
         print(data)
         return data
     else:
@@ -25,3 +33,5 @@ def conduct_bubble_sort(data):
 if __name__ == "__main__":
     test_list = [34, 12, 45, 32, 10, 6]
     sorted_list = conduct_bubble_sort(data=test_list)
+    test_list_2 = [6, 8, 9, 10, 34]
+    conduct_bubble_sort(test_list_2)
