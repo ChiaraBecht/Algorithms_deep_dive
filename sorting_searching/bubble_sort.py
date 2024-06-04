7000341098fd54b824f21de83be56b9ce25f58d3
ChiaraBecht <c.becht@st.hanze.nl>
2024-06-04 09:46:48 +0200

    implemented bubble sort

---
+++ sorting_searching/bubble_sort.py
@@ -0,0 +1,27 @@
+"""
+__author__ = Chiara Becht
+credits: Michelle Lawson
+"""
+
+def conduct_bubble_sort(data):
+    """
+    :params:
+        data: list to be sorted
+    :returns:
+        data: sorted list
+    """
+    if type(data) == list:
+        print(data)
+        data_len = len(data)
+        for i in range(data_len - 1): # skip last pass where we only look on the first element
+            for j in range(0, data_len - i - 1): # shortening the comparisons omitting the last elements that where sorted already
+                if data[j] > data[j+1]:
+                    data[j], data[j+1] = data[j+1], data[j]
+        print(data)
+        return data
+    else:
+        return "data must be of type list"
+
+if __name__ == "__main__":
+    test_list = [34, 12, 45, 32, 10, 6]
+    sorted_list = conduct_bubble_sort(data=test_list)
