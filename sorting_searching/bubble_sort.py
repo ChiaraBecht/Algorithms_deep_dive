2e481bc48211bbe95cb85461e994d2abfac1d8c3
ChiaraBecht <c.becht@st.hanze.nl>
2024-11-24 14:53:22 +0100

    tried out new list and print out all i, j and their list values to understand it again

--- sorting_searching/bubble_sort.py
+++ sorting_searching/bubble_sort.py
@@ -14,8 +14,11 @@ def conduct_bubble_sort(data):
         print(data)
         data_len = len(data)
         for i in range(data_len - 1): # skip last pass where we only look on the first element
+            print("pass:", i)
             swap = {"yes": 0, "no": 0}
             for j in range(0, data_len - i - 1): # shortening the comparisons omitting the last elements that where sorted already
+                print("j, j+1", j, j+1)
+                print("list items", data[j], data[j+1])
                 if data[j] > data[j+1]:
                     swap["yes"] += 1
                     data[j], data[j+1] = data[j+1], data[j]
@@ -34,3 +37,5 @@ if __name__ == "__main__":
     sorted_list = conduct_bubble_sort(data=test_list)
     test_list_2 = [6, 8, 9, 10, 34]
     conduct_bubble_sort(test_list_2)
+    test_list_3 = [20, 1, 19, 17, 2, 5, 18]
+    conduct_bubble_sort(test_list_3)
